4a006af885489c989b55a706eb61097c82c1e412
Jefry Romero <romerojefry@icloud.com>
2020-11-11 20:17:51 -0800

    starter template and gutting out past activity

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Cardd from "./components/Cardd";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Dogs from "./Dogs.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.Dogs to the Dogs json array
+  state = {
+    Dogs: Dogs
+  };
+
+  removeDogs = id => {
+    // Filter this.state.Dogs for Dogs with an id not equal to the id being removed
+    const Dogs = this.state.Dogs.filter(Dogs => Dogs.id !== id);
+    // Set this.state.Dogs equal to the new Dogs array
+    this.setState({ Dogs: Dogs });
+  };
+
+  // Map over this.state.Dogs and render a Cardd component for each Dogs object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Dogs List</Title>
+        {this.state.Dogs.map(Dogs => (
+          <Cardd
+            
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
---
+++ clickygame/src/components/Cardd/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Cardd(props) {
+  return (
+   console
+  );
+}
+
+export default FriendCard;
