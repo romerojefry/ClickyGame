d911713d13ae792d2349842a9118c642cd870232
Jefry Romero <romerojefry@icloud.com>
2020-11-11 21:02:20 -0800

    adding suffle array method on wrapper file

--- clickygame/src/components/Wrapper/index.js
+++ clickygame/src/components/Wrapper/index.js
@@ -4,5 +4,11 @@ import "./style.css";
 function Wrapper(props) {
   return <div className="wrapper">{props.children}</div>;
 }
-
+shuffleArray = Dogs => {
+  for (let i = Dogslength - 1; i> 0; i--){
+    const j = Math.floor(Math.random() * (i+1));
+    [Dogs[i], Dogs[j]] = [Dogs[j], Dogs[i]];
+  }
+  return (Dogs);
+}; 
 export default Wrapper;
